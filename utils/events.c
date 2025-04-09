9100db279f1566bf27785648c3a824a56d74d495
ismail0ka <ismail.bouchaib1@protonmail.com>
2025-04-09 00:56:55 +0000

    finally zoom works

--- utils/events.c
+++ utils/events.c
@@ -6,14 +6,14 @@
 /*   By: ikarouat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 22:56:20 by ikarouat          #+#    #+#             */
-/*   Updated: 2025/04/09 00:37:27 by ikarouat         ###   ########.fr       */
+/*   Updated: 2025/04/09 00:53:06 by ikarouat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <stdio.h>
 
-static void	zoom_in(t_fractal *fractal, double zoom_factor, int x, int y)
+/*static void	zoom_in(t_fractal *fractal, double zoom_factor, int x, int y)
 {
 	printf("Zoom in: %f\n", fractal->zoom);
 	fractal->zoom *= zoom_factor;
@@ -27,6 +27,41 @@ static void	zoom_out(t_fractal *fractal, double zoom_factor, int x, int y)
 	fractal->zoom /= zoom_factor;
 	fractal->o.real -= (x - WIDTH / 2.0) * 4.0 / (WIDTH * fractal->zoom);
 	fractal->o.imaginary -= (y - HEIGHT / 2.0) * 4.0 / (HEIGHT * fractal->zoom);
+}*/
+static void zoom_in(t_fractal *fractal, double zoom_factor, int x, int y)
+{
+    double aspect_ratio = (double)WIDTH / (double)HEIGHT;
+    
+    // Save the complex coordinates under the mouse before zooming
+    double mouse_re = (x - WIDTH / 2.0) * 4.0 / (WIDTH / fractal->zoom) + fractal->o.real;
+    double mouse_im = (y - HEIGHT / 2.0) * 4.0 / (HEIGHT / fractal->zoom * aspect_ratio) + fractal->o.imaginary;
+    
+    // Adjust zoom level
+    fractal->zoom *= zoom_factor;
+    
+    // Recalculate the origin to keep the point under the mouse at the same complex position
+    fractal->o.real = mouse_re - (x - WIDTH / 2.0) * 4.0 / (WIDTH / fractal->zoom);
+    fractal->o.imaginary = mouse_im - (y - HEIGHT / 2.0) * 4.0 / (HEIGHT / fractal->zoom * aspect_ratio);
+    
+    printf("Zoom in: %f\n", fractal->zoom);
+}
+
+static void zoom_out(t_fractal *fractal, double zoom_factor, int x, int y)
+{
+    double aspect_ratio = (double)WIDTH / (double)HEIGHT;
+    
+    // Save the complex coordinates under the mouse before zooming
+    double mouse_re = (x - WIDTH / 2.0) * 4.0 / (WIDTH / fractal->zoom) + fractal->o.real;
+    double mouse_im = (y - HEIGHT / 2.0) * 4.0 / (HEIGHT / fractal->zoom * aspect_ratio) + fractal->o.imaginary;
+    
+    // Adjust zoom level
+    fractal->zoom /= zoom_factor;
+    
+    // Recalculate the origin to keep the point under the mouse at the same complex position
+    fractal->o.real = mouse_re - (x - WIDTH / 2.0) * 4.0 / (WIDTH / fractal->zoom);
+    fractal->o.imaginary = mouse_im - (y - HEIGHT / 2.0) * 4.0 / (HEIGHT / fractal->zoom * aspect_ratio);
+    
+    printf("Zoom out: %f\n", fractal->zoom);
 }
 
 int	handle_mouse(int button, int x, int y, t_fractal *fractal)
